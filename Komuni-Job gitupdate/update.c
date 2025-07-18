a6f1014b91877b7ad212830479fa16206fa855b6
B0T-Josh <168722376+B0T-Josh@users.noreply.github.com>
2025-07-18 13:47:46 +0800

    Basic File Structure

---
+++ Komuni-Job gitupdate/update.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <windows.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+bool changeDir();
+bool addChange();
+bool commit();
+bool pullOrigin();
+bool upload();
+
+int main() {
+    if(addChange()) {
+        printf("Failed to change directory");
+        return 1;
+    }
+    else {
+        if(commit()) {
+            printf("Commit changes failed");
+            return 1;
+        }
+        else {
+            if(upload()) {
+                printf("Updating failed");
+                return 1;
+            }
+            else printf("Program updated");
+        }
+    }
+}
+
+bool pullOrigin() {
+    if(system("git pull origin main")) {
+        return true;
+    }
+    else return false;
+}
+
+bool addChange() {
+    if(system("git add .")) {
+        return true;
+    }
+    else return false;
+}
+
+bool upload() {
+    if(system("git push origin main --force")) {
+        return true;
+    }
+    else return false;
+}
+
+bool commit() {
+    if(system("git commit -m \"Basic File Structure\"")) {
+        return true;
+    }
+    else return false;
+}
+
+bool changeDir() {
+    if(chdir("C:\\Users\\jbsan\\Documents\\Program\\WEB DEV\\Komuni-Job")) {
+        return true;
+    }
+    else return false;
+}
