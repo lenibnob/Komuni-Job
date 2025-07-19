37cf0cb017a2e8b77f202980fb236de5edb4c005
B0T-Josh <168722376+B0T-Josh@users.noreply.github.com>
2025-07-19 12:50:28 +0800

    Basic File Structure

--- Komuni-Job gitupdate/update.c
+++ Komuni-Job gitupdate/update.c
@@ -2,38 +2,40 @@
 #include <windows.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool addChange();
 bool commit();
-// bool pullOrigin();
+bool pullOrigin();
 bool upload();
 
-int main() {
-    if(addChange()) {
-        printf("Failed to change directory");
-        return 1;
-    }
-    else {
-        if(commit()) {
-            printf("Commit changes failed");
-            return 1;
-        }
-        else {
-            if(upload()) {
-                printf("Updating failed");
-                return 1;
-            }
-            else printf("Program updated");
-        }
-    }
+int main(int argc, char *argv[]) {
+    if(argc > 1) {
+        if((strcmp(argv[1], "pull")) == 0) {
+            if(pullOrigin) {
+                printf("Local repository updated");
+            } else printf("There is an error pulling the main repository"); return 1;
+        } 
+        else if((strcmp(argv[1], "push")) == 0) {
+            if(addChange()) {
+                if(commit()) {
+                    if(upload()) {
+                        printf("Uploaded to git repository");
+                    } else printf("There is an error pushing to main repository"); return 1;
+                } else printf("There is an error committing changes"); return 1;
+            } else printf("There is an error adding changes"); return 1;
+        } else printf("Syntax error.\nupdate <pull>\nupdate <push>"); return 1; 
+    } else printf("Syntax error.\nupdate <pull>\nupdate <push>"); return 1;
 }
 
-// bool pullOrigin() {
-//     if(system("git pull origin main")) {
-//         return true;
-//     }
-//     else return false;
-// }
+bool pullOrigin() {
+    if(system("git fetch origin")) {
+        if(system("git merge origin/main")) {
+            return true;
+        }
+    }
+    return false;
+}
 
 bool addChange() {
     if(system("git add .")) {
