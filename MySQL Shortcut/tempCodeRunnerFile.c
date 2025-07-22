699fb0d6b19b18b31503ec434390355806f9e0aa
B0T-Josh <168722376+B0T-Josh@users.noreply.github.com>
2025-07-22 15:42:31 +0800

    Basic File Structure

--- MySQL Shortcut/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-if(strcmp(argv[1], "run")) {
-        //     if(change()) {
-        //         if(run()) {
-        //             printf("Running");
-        //         } else printf("Error on running");
-        //     } else printf("Error on change directory"); return 1;
-        // } el
