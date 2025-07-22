d4d0635291a0efcf8e3f44aa04be33290d7472d0
B0T-Josh <168722376+B0T-Josh@users.noreply.github.com>
2025-07-22 15:39:54 +0800

    MySQL Shortcut Commands

---
+++ MySQLShortcut/tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+if(strcmp(argv[1], "run")) {
+        //     if(change()) {
+        //         if(run()) {
+        //             printf("Running");
+        //         } else printf("Error on running");
+        //     } else printf("Error on change directory"); return 1;
+        // } el
