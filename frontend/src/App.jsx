2075bad7182ce8749a439189a2b36506c9e135e5
ItsActuallyZee <lancelotkarreon@gmail.com>
2025-07-28 14:41:48 +0800

    frontend redo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import './css/App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Favorites from "./pages/Favorites";
 import LoginPage from "./pages/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage";
 
@@ -15,7 +14,6 @@ export default function App() {
         
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/register" element={<RegistrationPage />} ></Route>
         </Routes>
