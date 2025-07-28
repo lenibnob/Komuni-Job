a8fa4f5d762f119607bf8754f709e67e4c38cadd
B0T-Josh <168722376+B0T-Josh@users.noreply.github.com>
2025-07-28 16:05:30 +0800

    update.c changed

--- Komuni-Job gitupdate/tempCodeRunnerFile.c
+++ Komuni-Job gitupdate/tempCodeRunnerFile.c
@@ -1,4 +1,72 @@
+#include <stdio.h>
+#include <windows.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 
-    //     return true;
-    // }
-    // else return false;
+bool addChange(char *file);
+bool commit(char *comment);
+bool pullOrigin();
+bool upload();
+
+int main(int argc, char *argv[]) {
+    if(argc > 1) {
+        for(int i = 1; i < argc; i++) {
+            if(strcmp(argv[i], "-a")) {
+                if(addChange(argv[i+1])) {
+                    printf("Change added");
+                } else printf("Failed to add changes"); return 1;
+            } else if(strcmp(argv[i], "-c")) {
+                if(commit(argv[i+1])) {
+                    printf("Commit changes successful");
+                } else printf("Failed to commit changes"); return 1;
+            } else if(strcmp(argv[i], "-p")) {
+                if(upload()) {
+                    printf("Push successful");
+                } else printf("Failed to push"); return 1;
+            } else {
+                printf("Syntax error.\nupdate <command> <option>\n"); 
+                printf("command - ['-a', '-c', '-p']\n");
+                printf("option - if -a is chosen, type the name of the file that you wanted to add changes into.\n");
+                printf("       - if -c is chosen, type the message for the commit.\n");
+                printf("       - if -p is chosen, you don't need to type anything.\n");
+                printf("usage:\nupdate -a <filename> / . (to add all changes)\n");
+                printf("update -a <filename> / . (to add all changes)\n");
+                printf("update -c <comment/message>\n");
+                printf("update -p\n");
+                printf("proper usage:\nupdate -a <filename/.> -c <comment/message> -p\n");
+            }
+        }
+    } else printf("Syntax error.\nupdate <pull>\nupdate <push> <comment>"); return 1;
+}
+
+bool pullOrigin() {
+    if(system("git fetch origin") == 0) {
+        if(system("git merge origin/main")) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool addChange(char *file) {
+    char command[256];
+    snprintf(command, 256, "git add \"%s\"", file);
+    if(system(command) == 0) {
+        return true;
+    } else return false;
+}
+
+bool upload() {
+    if(system("git push origin main") == 0) {
+        return true;
+    } else return false;
+}
+
+bool commit(char *comment) {
+    char command[256];
+    snprintf(command, 256, "git commit -m \"%s\"", comment);
+    if(system(command) == 0) {
+        return true;
+    } else return false;
+}
--- Komuni-Job gitupdate/update.c
+++ Komuni-Job gitupdate/update.c
@@ -4,43 +4,98 @@
 #include <stdbool.h>
 #include <string.h>
 
-bool addChange();
+bool addChange(char *file);
 bool commit(char *comment);
 bool pullOrigin();
+bool merge();
 bool upload();
 
 int main(int argc, char *argv[]) {
     if(argc > 1) {
-        if((strcmp(argv[1], "pull")) == 0) {
-            if(pullOrigin()) {
-                printf("Local repository updated");
-            } else printf("No need for further update"); return 1;
-        } 
-        else if((strcmp(argv[1], "push")) == 0) {
-            if(addChange()) {
-                if(argc > 2) {
-                    if(commit(argv[2])) {
-                        if(upload()) {
-                            printf("Uploaded to git repository");
-                        } else printf("There is an error uploading to the git repository"); return 1;
-                    } else printf("There is an error committing changes"); return 1;
-                } else printf("Syntax error.\nupdate <pull>\nupdate <push> <comment>");
-            } else printf("There is an error adding changes"); return 1;
-        } else printf("Syntax error.\nupdate <pull>\nupdate <push> <comment>"); return 1; 
-    } else printf("Syntax error.\nupdate <pull>\nupdate <push> <comment>"); return 1;
+        for(int i = 1; i < argc; i++) {
+            if(strcmp(argv[i], "-f") == 0) {
+                if(pullOrigin()) {
+                    printf("Fetch successful\n"); 
+                } else printf("Failed to fetch"); return 1;
+            } else if(strcmp(argv[i], "-m") == 0) {
+                if(merge()) {
+                    printf("Merge successful\n");
+                } else printf("Failed to merge"); return 1;
+            } else if(strcmp(argv[i], "-a") == 0) {
+                if(addChange(argv[i+1])) {
+                    printf("Change added\n");
+                } else printf("Failed to add changes\n"); return 1;
+            } else if(strcmp(argv[i], "-c") == 0) {
+                if(commit(argv[i+1])) {
+                    printf("Commit changes successful\n");
+                } else printf("Failed to commit changes\n"); return 1;
+            } else if(strcmp(argv[i], "-p") == 0) {
+                if(upload()) {
+                    printf("Push successful\n");
+                } else printf("Failed to push to origin main\n"); return 1;
+            } else {
+                printf("Syntax error.\nupdate <command> <option>\n"); 
+                printf("command - ['-a', '-c', '-p']\n");
+                printf("option - if -a is chosen, type the name of the file that you wanted to add changes into.\n");
+                printf("       - if -c is chosen, type the message for the commit.\n");
+                printf("       - if -p is chosen, you don't need to type anything after it. this pushes your updates to the remote branch\n");
+                printf("       - if -f is chosen, you don't need to type anything after it. this fetches updates from the remote branch\n");
+                printf("       - if -m is chosen, you don't need to type anything after it. this merges your local repository with the updates from the remote branch\n");
+                printf("usage:\nupdate -a <filename> / . (to add all changes)\n");
+                printf("update -a <filename> / . (to add all changes)\n");
+                printf("update -c <comment/message>\n");
+                printf("update -p\n");
+                printf("update -f\n");
+                printf("update -m\n");
+                printf("proper usage:\n");
+                printf("update -f -m\n");
+                printf("update -a <filename/.> -c <comment/message> -p\n");
+                printf("\nImportant note:\nMake sure to fetch and merge before you work on any file.\n");
+                printf("Push everytime you finish a file\n");
+                return 1;
+            }
+        }
+    } else {
+            printf("Syntax error.\nupdate <command> <option>\n"); 
+                printf("command - ['-a', '-c', '-p']\n");
+                printf("option - if -a is chosen, type the name of the file that you wanted to add changes into.\n");
+                printf("       - if -c is chosen, type the message for the commit.\n");
+                printf("       - if -p is chosen, you don't need to type anything after it. this pushes your updates to the remote branch\n");
+                printf("       - if -f is chosen, you don't need to type anything after it. this fetches updates from the remote branch\n");
+                printf("       - if -m is chosen, you don't need to type anything after it. this merges your local repository with the updates from the remote branch\n");
+                printf("usage:\nupdate -a <filename> / . (to add all changes)\n");
+                printf("update -a <filename> / . (to add all changes)\n");
+                printf("update -c <comment/message>\n");
+                printf("update -p\n");
+                printf("update -f\n");
+                printf("update -m\n");
+                printf("proper usage:\n");
+                printf("update -f -m\n");
+                printf("update -a <filename/.> -c <comment/message> -p\n");
+                printf("\nImportant note:\nMake sure to fetch and merge before you work on any file.\n");
+                printf("Push everytime you finish a file\n");
+            return 1;
+    }
 }
 
 bool pullOrigin() {
     if(system("git fetch origin") == 0) {
-        if(system("git merge origin/main")) {
-            return true;
-        }
+        return true;
     }
     return false;
 }
 
-bool addChange() {
-    if(system("git add .") == 0) {
+bool merge() {
+    if(system("git merge origin/main") == 0) {
+        return true;
+    }
+    return false;
+}
+
+bool addChange(char *file) {
+    char command[256];
+    snprintf(command, 256, "git add \"%s\"", file);
+    if(system(command) == 0) {
         return true;
     } else return false;
 }
