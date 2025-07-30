448a9108af83fa6cfe6be52092aa1e21c2d51ecc
B0T-Josh <168722376+B0T-Josh@users.noreply.github.com>
2025-07-30 16:39:29 +0800

    Update manage.c

--- backend/manage.c
+++ backend/manage.c
@@ -1,16 +1,34 @@
 #include <windows.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
+bool run(char *option);
+
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: manage <command>\n");
+    if (argc > 1) {
+        if(run(argv[1])) {
+            printf("Command executed successfully");
+        } else {
+            printf("an error has occured");
+            return 1;
+        }
+    } else {
+        printf("Syntax error.\nmanage <command>\n\n");
+        printf("Command:\n");
+        printf("manage runserver //Start a local server\n");
+        printf("manage makeMigrations //Make changes available for merging\n");
+        printf("manage migrate //Execute migration\n");
+        printf("manage flush //Deletes all data from all tables\n");
         return 1;
     }
-    
-    char command[256]; 
-    snprintf(command, sizeof(command), "python manage.py %s", argv[1]);
-
-    system(command);
     return 0;
+}
+
+bool run(char *option) {
+    char command[256]; 
+    snprintf(command, sizeof(command), "python manage.py %s", option);
+    if(system(command) == 0) {
+        return true;
+    } else return false;
 }
