6c305ba5ec49fbe736e02ec1c188c11e73618659
B0T-Josh <168722376+B0T-Josh@users.noreply.github.com>
2025-08-02 13:44:56 +0800

    Manage.c update

--- backend/manage.c
+++ backend/manage.c
@@ -3,11 +3,18 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-bool run(char *option);
+bool run(char *option[], int args);
 
 int main(int argc, char *argv[]) {
-    if (argc > 1) {
-        if(run(argv[1])) {
+    if (argc < 3 && argc > 1) {
+        if(run(argv, argc)) {
+            printf("Command executed successfully");
+        } else {
+            printf("an error has occured");
+            return 1;
+        }
+    } else if(argc > 2) {
+        if(run(argv, argc)) {
             printf("Command executed successfully");
         } else {
             printf("an error has occured");
@@ -17,7 +24,8 @@ int main(int argc, char *argv[]) {
         printf("Syntax error.\nmanage <command>\n\n");
         printf("Command:\n");
         printf("manage runserver //Start a local server\n");
-        printf("manage makemigrations //Make changes available for merging\n");
+        printf("manage makemigrations //Make changes available for migration\n");
+        printf("manage makemigrations <app_name>//Make specific changes available for migration\n");
         printf("manage migrate //Execute migration\n");
         printf("manage flush //Deletes all data from all tables\n");
         return 1;
@@ -25,10 +33,19 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-bool run(char *option) {
+bool run(char *option[], int args) {
     char command[256]; 
-    snprintf(command, sizeof(command), "python manage.py %s", option);
-    if(system(command) == 0) {
-        return true;
-    } else return false;
+    snprintf(command, sizeof(command), "python manage.py %s", option[1]);
+    if(args < 3) {
+        if(system(command) == 0) {
+            return true;
+        } else return false;
+    } else {
+        for(int i = 2; i < args; i++) {
+            snprintf(command + strlen(command), sizeof(command) - strlen(command), "%s ", option[i]);
+        }
+        if(system(command) == 0) {
+            return true;
+        } else return false;
+    }
 }
