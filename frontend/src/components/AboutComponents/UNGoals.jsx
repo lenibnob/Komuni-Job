d62fa5db5fd7abf0ee3860933592e81e0cf5e0bc
Zen <markdavidpangilinan111@gmail.com>
2025-08-05 23:52:50 +0800

    updated about us page

--- frontend/src/components/AboutComponents/UNGoals.jsx
+++ frontend/src/components/AboutComponents/UNGoals.jsx
@@ -4,13 +4,13 @@ export default function UNGoals() {
             <div className="unGoalsContainer">
                 <h2>Aligned UN Sustainable Development Goals</h2>
                 <div className="unCardsContainer">
-                    <div className="unCard"></div>
-                    <div className="unCard"></div>
-                    <div className="unCard"></div>
+                    <div className="unCard1"></div>
+                    <div className="unCard2"></div>
+                    <div className="unCard3"></div>
                 </div>
                 <div className="unCardsContainer">
-                    <div className="unCard"></div>
-                    <div className="unCard"></div>
+                    <div className="unCard4"></div>
+                    <div className="unCard5"></div>
                 </div>
             </div>
         </div>
