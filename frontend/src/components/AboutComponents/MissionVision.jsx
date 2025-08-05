287fc24a7603c494a5fb802f4607a5a3bbe3e988
Zen <markdavidpangilinan111@gmail.com>
2025-08-05 22:28:31 +0800

    about us update

--- frontend/src/components/AboutComponents/MissionVision.jsx
+++ frontend/src/components/AboutComponents/MissionVision.jsx
@@ -12,14 +12,14 @@ export default function MissionVision() {
         <div className="missionVisionBackground">
             <div className="missionVisionContainer">
                 <div className="missionVisionHeader">
-                    <h2>{`Many freelancers, first-time job seekers, and skilled individuals
-                    struggle to find job opportunities not because they lack ability,
-                    but because they lack visibility, connections,
-                    or a trusted place to get started.`}</h2>
+                    <h2>{(`Many freelancers, first-time job seekers, and skilled 
+                        locals are struggling to find jobs not because they
+                        lack ability, but because they lack visibility,
+                        connections, or a trusted place to get started.`)}</h2>
                 </div>
                 <div className="missionVisionCardContainer">
-                    <Card text={{header: "MISSION", content: "A Philippines where every community member can safely, affordably, and with dignity find or offer help nearby."}}/>
-                    <Card text={{header: "VISION", content: "A connected community where skills meet needs, and people grow together professionally, personally, and as a community."}}/>    
+                    <Card text={{header: "MISSION", content: (<><div style={{ fontWeight: "normal"}}>A Philippines where<br/> every community<br/> member can safely,<br/> affordably, and with<br/> dignity find or offer<br/> help nearby.</div></>)}}/>
+                    <Card text={{header: (<><div style={{ fontWeight: "bold"}}/>VISION</>), content: (<><div style={{ fontWeight: "normal"}}> A connected <br/>community where <br/>skills meet needs,<br/> and people grow together<br/> professionally,<br/> personally, and as a<br/> community.</div></>)}}/>    
                 </div>
             </div>
         </div>
