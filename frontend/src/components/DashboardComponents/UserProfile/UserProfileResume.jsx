b7075fab6564a8031ca0192de24491d7f41f09b5
ItsActuallyZee <lancelotkarreon@gmail.com>
2025-08-08 17:11:48 +0800

    user profile turned into components

---
+++ frontend/src/components/DashboardComponents/UserProfile/UserProfileResume.jsx
@@ -0,0 +1,19 @@
+export default function UserProfileResume() {
+    return (
+        <div className="userProfileResumeContainer">
+            <div className="userProfileResume">
+                <h2>RESUMES</h2>
+                <div>
+                    <h2>no idea what to put here</h2>
+                </div>
+            </div>
+            <div className="userProfileFinishedJobs">
+                <h2>FINISHED JOB</h2>
+                <div>
+                    <h2>list here</h2>
+                    <h2>list here</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Dashboard/UserProfile.jsx
+++ frontend/src/pages/Dashboard/UserProfile.jsx
@@ -2,6 +2,7 @@ import "@/css/DashboardCSS/UserProfile.css"
 import DashboardNav from "@/components/DashboardComponents/DashboardNav";
 import UserProfileDetails from "@/components/DashboardComponents/UserProfile/UserProfileDetails";
 import UserProfileDescription from "@/components/DashboardComponents/UserProfile/UserProfileDescription";
+import UserProfileResume from "@/components/DashboardComponents/UserProfile/UserProfileResume";
 
 export default function UserProfile() {
     return (
@@ -14,22 +15,7 @@ export default function UserProfile() {
                 <div className="userProfile">
                     <UserProfileDetails />
                     <UserProfileDescription />
-
-                    <div className="userProfileResumeContainer">
-                        <div className="userProfileResume">
-                            <h2>RESUMES</h2>
-                            <div>
-                                <h2>no idea what to put here</h2>
-                            </div>
-                        </div>
-                        <div className="userProfileFinishedJobs">
-                            <h2>FINISHED JOB</h2>
-                            <div>
-                                <h2>list here</h2>
-                                <h2>list here</h2>
-                            </div>
-                        </div>
-                    </div>
+                    <UserProfileResume />
                 </div>
             </div>
         </div>
